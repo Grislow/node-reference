b2920045ad173d98ec728914831eb38f074aa2c8
Grzegorz Maj <gmaj@inbrackets.pl>
2019-03-17 14:09:50 +0100

    basic web server

--- app.js
+++ app.js
@@ -1,65 +1,28 @@
+const http = require('http');
 const fs = require('fs');
-const zlib = require('zlib');
 
-// ***************************************
-// Setting encoding
-var buffer8 = Buffer.from('Hello');
-var buffer16 = Buffer.from('Hello', 'utf16le');
+http.createServer((req, res) => {
 
-console.log(buffer8);
-console.log(buffer16);
+    res.writeHead(200, { 'Content-Type': 'text/html'});  
+    fs.createReadStream(__dirname + '/index.html', 'utf8').pipe(res);
 
-// common methods
-console.log(buffer8.toString());
-console.log(buffer8.toJSON());
-buffer8.write('J');
-console.log(buffer8.toString());
+}).listen(9000, () => console.log('Connected on port 9000'));
 
-// ***************************************
-// module objects
-console.log(__dirname);
-    // /Users/gregorymaj/projects/node-basics
-console.log(__filename);
-    // /Users/gregorymaj/projects/node-basics/app.js
+http.createServer((req, res) => {
+    
+    if(req.url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/html'});  
+        fs.createReadStream(__dirname + '/index.html', 'utf8').pipe(res);
+    } else if(req.url === '/api') {
+        res.writeHead(200, { 'Content-Type': 'application /json'});     
+        let json = {
+            firstName: 'John',
+            lastName: 'Smith'
+        }
+        res.end(JSON.stringify(json));
+    } else {
+        res.writeHead(404);
+        res.end();
+    }
 
-// reading a file synchronously
-const test = fs.readFileSync(__dirname + '/test.txt');
-
-console.log(test.toString());
-
-// reading a file asynchronously
-fs.readFile(__dirname + '/test.txt', (err, data) => {
-    console.log(data.toString());
-})
-
-// ***************************************
-// using a readable stream
-const readStream = fs.createReadStream(__dirname + '/long.txt', {
-    encoding: 'utf8',
-    highWaterMark: 2 * 1024
-});
-
-// using a writable stream
-const writeStream = fs.createWriteStream(__dirname + '/other.txt', {
-    encoding: 'utf8',
-    highWaterMark: 2 * 1024 // Stream size = 2 KB
-});
-
-// callback execution when buffer is filled
-readStream.on('data', (chunk) => {
-    writeStream.write(chunk);
-    writeStream.write('***************************************');
-})
-
-// using pipe for the above
-const writer = fs.createWriteStream(__dirname + '/pipeTo.txt');
-
-readStream.pipe(writer);
-
-// gzip a file
-const writeCompressed = fs.createWriteStream(__dirname + '/compress.txt.gz')
-
-const gzip = zlib.createGzip();
-
-readStream.pipe(gzip).pipe(writeCompressed);
-
+}).listen(9001, '127.0.0.1', () => console.log('Connected on port 9001'));
